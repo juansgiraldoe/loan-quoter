4f5309de6ea45528710b606e7c6bbb38d8503bd8
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-23 11:20:18 -0500

    [Update] - Add useState. // Creare input range.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 
 function App() {
+
+  const [ cantidad, setCantidad ] = useState(10000);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  function handleChange(e) {
+    setCantidad(+e.target.value);
+  }
 
   return (
 
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+      <input
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        type="range"
+        name="" 
+        id=""
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+        onChange={handleChange}
+      />
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
     </div>
 
   )
